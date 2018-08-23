b2b40c9a98133bf90358bc6f51d1d0218c2b4bda
utilisateurlanda <42026603+utilisateurlanda@users.noreply.github.com>
2018-08-23 16:35:02 +0200

    Mise en ligne de Horizon
    Bascule des fichiers horizon telecom

---
+++ jquery.js
@@ -0,0 +1,10 @@
+
+/*----------------------- fichier jQuery--------------------------------------------- */
+
+
+// Charge la dernière version de jQuery dans 1.xx.
+google.load('jquery','1');
+// Charge la dernière version de jQuery dans 1.4x.
+google.load('jquery','1.4');
+// Charge la version 1.4.1.
+google.load('jquery','1.4.1');
